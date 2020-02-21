cf318f694f072e4a25ac34b5908513ddef094513
Konstantinos Stavrianos <stam12176@gmail.com>
2020-02-21 11:58:32 +0200

    adding started project

---
+++ mlinputs.py
@@ -0,0 +1,160 @@
+from numpy import array
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers.core import Activation, Dropout, Dense
+from keras.layers import Flatten, LSTM
+from keras.layers import GlobalMaxPooling1D
+from keras.models import Model
+from keras.layers.embeddings import Embedding
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras.layers import Input
+from keras.layers.merge import Concatenate
+
+import pandas as pd
+import numpy as np
+import re
+
+
+reviews = pd.read_csv('train.csv')
+
+reviews.isnull().values.any()
+
+X = reviews.drop('target', axis=1)
+
+y = reviews['target']
+
+from sklearn import preprocessing
+
+# label_encoder object knows how to understand word labels.
+label_encoder = preprocessing.LabelEncoder()
+
+# Encode labels in column 'species'.
+y = label_encoder.fit_transform(y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+from keras.utils import to_categorical
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+def preprocess_text(sen):
+
+    # Remove punctuations and numbers
+    sentence = re.sub('[^a-zA-Z]', ' ', sen)
+
+    # Single character removal
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+
+    # Removing multiple spaces
+    sentence = re.sub(r'\s+', ' ', sentence)
+
+    return sentence
+
+X1_train = []
+sentences = list(X_train["text"])
+for sen in sentences:
+    X1_train.append(preprocess_text(sen))
+
+
+X1_test = []
+sentences = list(X_test["text"])
+for sen in sentences:
+    X1_test.append(preprocess_text(sen))
+
+
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(X1_train)
+
+X1_train = tokenizer.texts_to_sequences(X1_train)
+X1_test = tokenizer.texts_to_sequences(X1_test)
+
+vocab_size = len(tokenizer.word_index) + 1
+
+maxlen = 200
+
+X1_train = pad_sequences(X1_train, padding='post', maxlen=maxlen)
+X1_test = pad_sequences(X1_test, padding='post', maxlen=maxlen)
+
+from numpy import array
+from numpy import asarray
+from numpy import zeros
+
+embeddings_dictionary = dict()
+
+glove_file = open('glove.6B.100d.txt', encoding="utf8")
+
+for line in glove_file:
+    records = line.split()
+    word = records[0]
+    vector_dimensions = asarray(records[1:], dtype='float32')
+    embeddings_dictionary[word] = vector_dimensions
+
+glove_file.close()
+
+embedding_matrix = zeros((vocab_size, 100))
+for word, index in tokenizer.word_index.items():
+    embedding_vector = embeddings_dictionary.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[index] = embedding_vector
+
+
+
+
+Xx_train = pd.concat([X_train, X_test], axis=0)
+X2_train=pd.get_dummies(Xx_train, columns=['location', 'keyword'])
+X2_train = X2_train.drop(X2_train[["id","text"]], axis=1)
+X2_test = X2_train[6090:].values
+X2_train = X2_train[:6090].values
+
+
+input_1 = Input(shape=(maxlen,))
+
+input_2 = Input(shape=(3562,))
+
+embedding_layer = Embedding(vocab_size, 100, weights=[embedding_matrix], trainable=False)(input_1)
+LSTM_Layer_1 = LSTM(128)(embedding_layer)
+
+dense_layer_1 = Dense(100, activation='relu')(input_2)
+dense_layer_2 = Dense(100, activation='relu')(dense_layer_1)
+
+concat_layer = Concatenate()([LSTM_Layer_1, dense_layer_2])
+dense_layer_3 = Dense(10, activation='relu')(concat_layer)
+output = Dense(2, activation='sigmoid')(dense_layer_3)
+model = Model(inputs=[input_1, input_2], outputs=output)
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+print(model.summary())
+
+from keras.utils import plot_model
+plot_model(model, to_file='model_plot3.png', show_shapes=True, show_layer_names=True)
+
+history = model.fit(x=[X1_train, X2_train], y=y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.2)
+
+
+
+score = model.evaluate(x=[X1_test, X2_test], y=y_test, verbose=1)
+
+print("Test Score:", score[0])
+print("Test Accuracy:", score[1])
+
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train','test'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train','test'], loc='upper left')
+plt.show()
